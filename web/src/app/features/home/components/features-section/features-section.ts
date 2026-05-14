ea272e80c6dbfe22b654b5c5bf5441027369a5b1
inctifra <inctifra@gmail.com>
2026-05-14 16:14:06 +0300

    first commit v1.0.0

---
+++ web/src/app/features/home/components/features-section/features-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sacco-features-section',
+  imports: [],
+  templateUrl: './features-section.html',
+  styleUrl: './features-section.scss',
+})
+export class FeaturesSection {}
